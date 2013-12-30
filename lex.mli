e170ea869ef5caf7561ab8f1fb124d4dc643b794
Alvin Kerber <akerber47@gmail.com>
2013-12-30 09:41:24 -0800

    Wrote clean interface for lexer
    Lexer now has multiple exposed steps:
    
    source ---prelex--> ---postlex--> ---unlayout--> parser input
    
    prelex:
    - read in source, produce queue of lexemes
    - remove comments
    - detect lexing errors
    
    postlex:
    - identify reserved words and operators
    - copy lexeme contents
    - convert raw indices to line / column nums
    
    unlayout:
    - insert tokens based on whitespace
    [Haskell 98 Report, section 9.3]
    
    and 2 helper functions
    source ---compute_indents-->      indent level of each line (for unlayout)
    source ---compute_line_and_col--> line and col nums (for error reporting)
    
    Not yet implemented.

--- lex.mli
+++ lex.mli
@@ -1,3 +1,5 @@
+open Batteries
+
 type token =
   | QVarId
   | QConId
@@ -8,18 +10,55 @@ type token =
   | CharLit
   | StringLit
   | Special
+  (* TODO, many more token types needed for parsing. *)
+
+type pretoken =
+  | PreQVarId
+  | PreQConId
+  | PreQVarSym
+  | PreQConSym
+  | PreIntLit
+  | PreFloatLit
+  | PreCharLit
+  | PreStringLit
+  | PreSpecial
 
 type lexeme = {
-  token     : token;  (* token type of the lexeme *)
-  startline : int;    (* Line of the source string the lexeme starts on *)
-  endline   : int;    (* and ends on. Only strings can be multi line *)
-  startpos  : int;    (* in source string, starting position ... *)
-  endpos    : int;    (* ... and ending position of the lexeme. Following
-                           C conventions endpos is after last char *)
+  token     : token;
+  contents  : string;
+  (* Line and column for error reporting purposes. Tokens added in the
+   * layout-handling step have column -1 (but the appropriate line #). *)
+  startline : int;    (* Line of the source string the lexeme starts on (and
+                         ends on, unless it's a multiline string literal) *)
+  startcol  : int;    (* Column ... *)
 }
 
-type lexeme_stream
+type prelexeme = {
+  pretoken  : pretoken;
+  startix   : int;    (* Starting index in the raw (unsplit) source string *)
+  endix     : int;    (* Ending index, points *after* last char. *)
+}
 
-val lex : string -> lexeme_stream
+(** Split the given input program on line breaks, and compute the indent level
+ *  of each line. Uses \r\n if found, \r or \n otherwise. *)
+val compute_indents : string -> int Array.t
 
-(* val next_lexeme : lexeme_stream -> lexeme Option.t *)
+(** Given a raw index into a string, computes the corresponding line number and
+ * column number (both starting at 1) in the string. If index is an line
+ * breaking character, return the line number before the break, and a column
+ * number 1 beyond the last "actual" column. *)
+val compute_line_and_col : string -> int -> int * int
+
+(** Tokenize the input program, ignoring all whitespace and using broad
+ * categories of tokens. *)
+val prelex : string -> prelexeme Queue.t
+
+(** Converts tokens to their final form, ready for parsing, by further
+ * categorizing them and by copying their contents out of the raw string. *)
+val postlex : string -> prelexeme Queue.t -> lexeme Queue.t
+
+(** Use the layout algorithm to insert tokens appropriately for the whitespace
+ * in the original program. After this point we no longer need any whitespace
+ * information. Input is the token stream before insertion, and the indent
+ * level of each line of the source program. *)
+val unlayout : lexeme Queue.t -> int Array.t -> lexeme Queue.t
