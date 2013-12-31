127c2e83dd6e1404b085a0e03e5e3fb9bf946e32
Alvin Kerber <akerber47@gmail.com>
2013-12-30 19:41:39 -0800

    Fixed lots of small typos
    Still haven't actually run the code (in fact, it doesn't even link correctly)

--- lex.ml
+++ lex.ml
@@ -98,7 +98,7 @@ let isspecial = function
 let rec has_comment_start = function
   | [] -> true
   | '-'::cs -> has_comment_start cs
-  | c::cs -> not (issymb c)
+  | c::_ -> not (issymb c)
 ;;
 
 let compute_indents s =
@@ -112,8 +112,8 @@ let compute_indents s =
    * code. Screw it. *)
   let compute_indent line =
     let rec loop i ind =
-      if i < String.length s then
-        match s.[i] with
+      if i < String.length line then
+        match line.[i] with
         (* Vertical tabs count as 1 white char *)
         | '\x0b'
         | ' '  -> loop (i+1) (ind+1)
@@ -229,20 +229,23 @@ let prelex src_string =
         { pretoken = PreCharLit; startix = i; endix = -1; })
     | '"'::cs, Default -> do_nextchar (i+1) cs (InLexeme
         { pretoken = PreStringLit; startix = i; endix = -1; })
-    | _::cs, Default -> raise (Lex_error (i, "Invalid character"))
+    (* This error case will also be reached if we're in the middle of a token /
+     * special state, because we'll first end the current token / state then
+     * call do_nextchar again on that char with default state. *)
+    | _, Default -> raise (Lex_error (i, "Invalid character"))
     (* This is where things get complicated. *)
     | css, (InLexeme plx) -> begin
       match css,plx.pretoken with
       (* Escape sequences and end characters in chars/strings *)
       | '\\'::cs, (PreCharLit | PreStringLit) -> do_escape (i+1) cs plx
-      | c::cs, PreCharLit where islinebreak c ->
+      | c::_, PreCharLit when islinebreak c ->
           raise (Lex_error (i, "Unmatched '"))
       | '\''::cs, PreCharLit -> begin
-        endtok {plx with endix = i+1};
+        endlexeme {plx with endix = i+1};
         do_nextchar (i+1) cs Default
       end
       | '"'::cs, PreStringLit -> begin
-        endtok {plx with endix = i+1};
+        endlexeme {plx with endix = i+1};
         do_nextchar (i+1) cs Default
       end
       (* Dot in int literal, followed by digit -> float literal *)
@@ -267,7 +270,7 @@ let prelex src_string =
        * processing the next character, just end the lexeme and go back to
        * the top for this character. *)
       | _, _ -> begin
-        endtok {plx with endix = i};
+        endlexeme {plx with endix = i};
         do_nextchar i css Default
       end
     end
@@ -279,14 +282,14 @@ let prelex src_string =
         do_nextchar (i+1) cs (InHexInt n)
     | css, (InOctInt n)
     | css, (InHexInt n) -> begin
-      endtok { pretoken = PreIntLit; startix = n; endix = i };
+      endlexeme { pretoken = PreIntLit; startix = n; endix = i };
       do_nextchar i css Default
     end
     (* Continuation and end of floatexps *)
     | c::cs, (InFloatExp n) when isdigit c ->
         do_nextchar (i+1) cs (InFloatExp n)
     | css, (InFloatExp n) -> begin
-      endtok { pretoken = PreFloatLit; startix = n; endix = i };
+      endlexeme { pretoken = PreFloatLit; startix = n; endix = i };
       do_nextchar i css Default
     end
     (* Continuation and end / conversion of modids *)
@@ -294,13 +297,13 @@ let prelex src_string =
         do_nextchar (i+1) cs (InModId n)
     | '.'::(c::cs), (InModId n) when issymb c ->
         do_nextchar (i+2) cs (InLexeme
-        { pretoken = PreQVarSym, startix = n, endix = -1 })
+        { pretoken = PreQVarSym; startix = n; endix = -1 })
     | '.'::(c::cs), (InModId n) when islower c || isupper c ->
         do_nextchar (i+2) cs (InLexeme
-        { pretoken = PreQVarId, startix = n, endix = -1 })
+        { pretoken = PreQVarId; startix = n; endix = -1 })
     (* ModId which doesn't qualify a name is really a PreQVarId *)
     | css, (InModId n) -> begin
-      endtok { pretoken = PreQVarId, startix = n, endix = i };
+      endlexeme { pretoken = PreQVarId; startix = n; endix = i };
       do_nextchar i css Default
     end
   (* do_escape : int -> char list -> prelexeme -> prelexeme Queue.t
@@ -311,20 +314,21 @@ let prelex src_string =
     let rec do_escape_gap i css plx =
       match css with
       | [] -> raise (Lex_error (i, "Unmatched \""))
-      | '\'::cs -> do_nextchar (i+1) cs plx
+      | '\\'::cs -> do_nextchar (i+1) cs (InLexeme plx)
       | c::cs when iswhite c -> do_escape_gap (i+1) cs plx
       | _ -> raise (Lex_error (i, "Non-whitespace character in gap"))
     and escape_while_pred pred i css plx =
       match css with
       | [] -> raise (Lex_error (i, "Unmatched \""))
       | c::cs when pred c -> escape_while_pred pred (i+1) cs plx
-      | css -> do_nextchar i css plx
+      | css -> do_nextchar i css (InLexeme plx)
+    in
     match css with
     | [] -> raise (Lex_error (i, "Unterminated escape sequence"))
     | c::cs -> begin
       match c with
       (* 1 character escape sequences *)
-      | 'a' | 'b' | 'f' | 'n' | 'r' | 't' | 'v' | '\' | '\'' | '"' | '&'
+      | 'a' | 'b' | 'f' | 'n' | 'r' | 't' | 'v' | '\\' | '\'' | '"' | '&'
       (* escaped ascii control characters *)
       | '\x00' .. '\x1f' -> do_nextchar (i+1) cs (InLexeme plx)
       (* Numeric escapes *)
@@ -377,7 +381,7 @@ let prelex src_string =
       end
     end
   (* To end lexeme, just add it to the queue. *)
-  and endlexeme = Queue.enqueue prelexemes
+  and endlexeme plx = Queue.add plx prelexemes
   in do_nextchar 0 src_chars Default
 ;;
 
@@ -386,141 +390,3 @@ let postlex s q = Queue.create ()
 
 let unlayout q a = Queue.create ()
 ;;
-
-(*
-  let rec do_nextline s =
-    (* Matching algorithm for escape sequences in strings. *)
-    let match_escape () = () in
-    (* Takes the current token, finishes it, and adds it to the queue. Starts a
-     * new token of the given type. If the token has fixed (known) length,
-     * slurp up the rest of it immediately. *)
-    let rec newtok tk =
-      (* Check for 1-line comments *)
-      if !curlexeme.token = QVarSym &&
-         is_comment_start (String.sub s
-                                      !curlexeme.startpos
-                                      (!i - !curlexeme.startpos)) then begin
-        (* Symbol token we were going to add actually indicates a start-of-line
-         * comment, so just modify the current token (and keep it current). *)
-        curlexeme := {!curlexeme with token = Comment};
-        (* If this is the end of our string, we then need to pop this comment
-         * immediately (means we got called from the top of do_nextline in the
-         * process of moving to next line of source) *)
-        if !i >= String.length s then
-          newtok NullToken
-      end
-      else begin
-        if !curlexeme.token <> NullToken &&
-           !curlexeme.token <> Comment &&
-           !curlexeme.token <> BlockComment then begin
-          curlexeme := {!curlexeme with endpos = !i; endline = !curline};
-          Queue.add !curlexeme lexemes
-        end;
-        curlexeme := { token = tk; startline = !curline; endline = -1;
-                       startpos = !i; endpos = -1 };
-        (* Check for token types we can process immediately *)
-        match tk with
-          (* Special tokens are only 1 character long *)
-          | Special -> begin
-            i := !i + 1;
-            newtok NullToken
-          end
-          (* Process character literals immediately *)
-          | CharLit -> begin
-            if !i+1 < String.length s then
-              i := !i + 1
-            else
-              lex_error "Unmatched '";
-            match s.[!i] with
-              | '\'' -> lex_error "Empty character literal"
-              (* TODO handle '\&' error case (0 width char literal). Maybe deal
-               * with this when parsing? *)
-              | '\\' -> match_escape ()
-              | _ -> ();
-            if !i+1 < String.length s then
-              i := !i + 1
-            else
-              lex_error "Unmatched '";
-            if s.[!i] <> '\'' then
-              lex_error "Character literal too long";
-            i := !i + 1;
-            newtok NullToken
-          end
-          | _ -> ();
-        ()
-      end
-    and nextchar_loop () =
-      if !i < String.length s then begin
-        match !curlexeme.token, s.[!i] with
-          (* If we're already inside a 1-line comment, nothing happens *)
-          | Comment, _ -> ()
-          (* Block comment start, note block comments DO nest *)
-          | _, '{' when (!curlexeme.token <> StringLit &&
-                         !i + 1 < String.length s && s.[!i+1] = '-') -> begin
-            blockcomment_depth := !blockcomment_depth + 1;
-            if !blockcomment_depth = 0 then
-              newtok BlockComment
-          end
-          (* Block comment end *)
-          | _, '-' when (!curlexeme.token <> StringLit &&
-                         !i + 1 < String.length s && s.[!i+1] = '}') -> begin
-            blockcomment_depth := !blockcomment_depth - 1;
-            if !blockcomment_depth = 0 then
-              newtok NullToken
-          end
-          (* If we're otherwise already inside a block comment, nothing. *)
-          | BlockComment, _ -> ()
-          (* Process the current character. Note that if the current token
-           * continues through this character, we do nothing at all in this
-           * matching step. We process fixed-length tokens (char literals and
-           * specials) immediately when they start, so curlexeme.token CANNOT
-           * be CharLit or Special at this point. *)
-          (* Whitespace or special chars ends the current token,
-           * except in string literals *)
-          | StringLit, c when iswhite(c) || isspecial(c) -> ()
-          | _, c when iswhite(c) -> newtok NullToken
-          | _, c when isspecial(c) -> newtok Special
-          (* Note that digits can appear within varids / conids. Otherwise
-           * these arbitrary-length tokens basically end / start whenever the
-           * character class changes. *)
-          | (NullToken | QVarSym | IntLit), ('_' | 'a' .. 'z') ->
-              newtok QVarId
-          | (NullToken | QVarSym | IntLit), ('A' .. 'Z') ->
-              newtok QConId
-          | (NullToken | QVarId | QConId | IntLit), c when issymb(c) ->
-              newtok QVarSym
-          | (NullToken | QVarSym), ('0' | '1' .. '9') ->
-              newtok IntLit
-          | (NullToken | QVarId | QConId | QVarSym | IntLit), '\'' ->
-              newtok CharLit
-          | (NullToken | QVarId | QConId | QVarSym | IntLit), '"' ->
-              newtok StringLit
-          (* String literals: watch out for escaping and end quotes *)
-          | StringLit, '"' -> newtok NullToken
-          | StringLit, '\\' -> match_escape ()
-          (* Otherwise, current token continues, so do nothing *)
-          | _ -> ();
-        i := !i + 1;
-        nextchar_loop ()
-      end
-    in
-    (* End any current token, unless it can span multiple lines. *)
-    if !curlexeme.token <> NullToken && !curlexeme.token <> StringLit &&
-       !curlexeme.token <> BlockComment then
-      newtok NullToken;
-    curline := !curline + 1;
-    i := 0;
-    nextchar_loop ()
-  in
-  (* Process the whole program, line by line *)
-  List.iter do_nextline lines;
-  (* Finish the last token (if any), and return *)
-  if !curlexeme.token = StringLit then
-    lex_error "Unmatched \"";
-  if !curlexeme.token = BlockComment then
-    lex_error "Unmatched (*";
-  curlexeme := {!curlexeme with endpos = !i; endline = !curline};
-  Queue.add !curlexeme lexemes;
-  lexemes
-;;
-*)
