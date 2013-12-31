84d49d8112c33dabeb3263fd822a9c02c7bb2b68
Alvin Kerber <akerber47@gmail.com>
2013-12-30 21:24:18 -0800

    Starting to add testing framework
    Added code to makefile to build it. Makefile needs to be rewritten to be
    remotely tolerable: figure out how ocaml build chain works.

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-all: lex.native
+all: lex.native test_lex.native
 
 lex.native: lex.ml lex.mli
 	ocamlfind ocamlc -w +a-4-44 -package batteries -linkpkg lex.mli lex.ml -o lex.native
 
+test_lex.native: lex.ml lex.mli test/test_lex.ml
+	ocamlfind ocamlc -w +a-4-44 -package batteries -package oUnit -linkpkg lex.mli lex.ml test/test_lex.ml -o test_lex.native
+
 clean:
-	$(RM) lex *.cmi *.cmo
+	$(RM) *.cmi *.cmo *.native
+	cd test && $(RM) *.cmi *.cmo *.native
 
---
+++ test/test_lex.ml
@@ -0,0 +1,29 @@
+open OUnit
+;;
+
+open Batteries
+;;
+
+(* Check that lexing an input string gives the correct tokens, at least. *)
+let assert_eqtoks toklist src =
+  assert_equal ~cmp:(List.eq (=))
+               ~printer:dump
+               toklist
+               (List.map (fun plx -> plx.Lex.pretoken)
+                         (List.of_enum (Queue.enum (Lex.prelex src))))
+;;
+
+let test1 ctxt = assert_eqtoks [] "";;
+
+let test2 ctxt = assert_eqtoks [Lex.PreQVarId ; Lex.PreSpecial] "x(";;
+
+(* Name the test cases and group them together *)
+let suite =
+  "suite">:::
+     ["test1">:: test1;
+       "test2">:: test2]
+;;
+
+let () =
+  run_test_tt_main suite; ()
+;;
