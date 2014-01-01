bec03d4ef26f0742a9ba9cbbce051ac5d01dc46b
Alvin Kerber <akerber47@gmail.com>
2014-01-01 14:53:47 -0800

    Wrote interface for parser generator
    Types for grammar productions and semantic actions.

---
+++ src/parser_gen.mli
@@ -0,0 +1,25 @@
+open Batteries
+
+(* Using pre-existing parser generators is LAME, so this is a hand-rolled one.
+ * What could possibly go wrong? *)
+
+type symbol =
+  | Terminal Lex.token
+  | Nonterminal Parse.nonterm
+
+type production = {
+  lhs : Parse.nonterm;
+  rhs : symbol list;
+  (* Semantic action to be applied to the list of return values of the
+   * semantic actions of the terms of rhs. *)
+  action : (Parse.cst list -> Parse.cst);
+}
+
+type grammar = {
+  productions : production list;
+  (* Semantic action to be applied when matching any terminal symbol. Taken as
+   * input the matched lexeme (to extract its contents, line/col #s, etc. *)
+  terminal_action : (Lex.lexeme -> Parse.cst);
+}
+
+val generate : grammar -> Lex.lexeme Queue.t -> Parse.cst
