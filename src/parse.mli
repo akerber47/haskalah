b74766e4731064287b6d1191ec2ae9f76a824180
Alvin Kerber <akerber47@gmail.com>
2014-01-01 15:10:17 -0800

    Wrote out all the nonterminals for grammar
    Writing the actual grammar is going to be such a pain.

---
+++ src/parse.mli
@@ -0,0 +1,81 @@
+open Batteries
+
+(* Build an abstract syntax tree for the given stream of lexemes. *)
+val parse_ast : Lex.lexeme Queue.t -> ast
+
+(* Nonterminal symbols in the Haskell 98 grammar. Long, ugly verbatim copy of
+ * section 9.5 of the Report. Note that we implement
+ * https://ghc.haskell.org/trac/haskell-prime/wiki/FixityResolution ie resolve
+ * precedence and fixities of operator expressions in a later step. This
+ * violates the Haskell 98 specification. *)
+type nonterm =
+  | NTmodule
+  | NTbody
+  | NTimpdecls
+  | NTexports
+  | NTexport
+  | NTimpdecl
+  | NTimpspec
+  | NTimport
+  | NTename
+  | NTtopdecls
+  | NTtopdecl
+  | NTdecls
+  | NTdecl
+  | NTcdecls
+  | NTcdecl
+  | NTidecls
+  | NTidecl
+  | NTgendecl
+  | NTops
+  | NTvars
+  | NTfixity
+  | NTtype
+  | NTbtype
+  | NTatype
+  | NTqtycon
+  | NTcontext
+  | NTclass
+  | NTscontext
+  | NTsimpleclass
+  | NTsimpletype
+  | NTconstrs
+  | NTconstr
+  | NTnewconstr
+  | NTfielddecl
+  | NTderiving
+  | NTdclass
+  | NTinst
+  | NTfunlhs
+  | NTrhs
+  | NTgdrhs
+  | NTgd
+  | NTexp
+  | NTinfixexp (* See note above *)
+  | NTexp10
+  | NTfexp
+  | NTaexp
+  | NTqual
+  | NTalts
+  | NTalt
+  | NTgdpat
+  | NTstmts
+  | NTstmt
+  | NTfbind
+  | NTpat
+  | NTinfixpat (* See note above *)
+  | NTpat10
+  | NTapat
+  | NTfpat
+  | NTgcon
+  | NTvar
+  | NTqvar
+  | NTcon
+  | NTqcon
+  | NTvarop
+  | NTqvarop
+  | NTconop
+  | NTqconop
+  | NTop
+  | NTqop
+  | NTqconsym
