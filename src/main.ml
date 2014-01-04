63efba2ece57f1df7dd94c90db18053a7d0db6e3
Alvin Kerber <akerber47@gmail.com>
2014-01-04 15:07:55 -0800

    Rewrote dummy code in main to call functor
    Calls Parser_gen.Make on a pretty stupid input module, but good enough.

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,21 @@
 open Batteries
 ;;
 
+module FooParserGenerator = Parser_gen.Make (
+  struct
+    type tm = int
+    let tm_compare = compare
+    type ntm = int
+    let ntm_compare = compare
+    type lx = int
+    let lx_to_tm = identity
+    type ast = int
+    let eof = 0
+  end
+);;
+
 let () = begin
-  let s = Parser_gen.ItemSet.empty in
+  let s = FooParserGenerator.ItemSet.empty in
   print_string (dump s);
   print_string (dump (List.of_enum (Queue.enum (Lex.prelex "foobar"))))
 end
