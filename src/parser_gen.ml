dacb658d20aab9d2b3d515e678979e88c5ce01e9
Alvin Kerber <akerber47@gmail.com>
2014-01-03 21:56:06 -0800

    Implemented computation of FIRST sets
    Unfortunately, code will totally crap out (infinite loop) if grammar is left
    recursive, and doesn't check for that ahead of time.
    
    Also, added even more functions to parser generator interface.

--- src/parser_gen.ml
+++ src/parser_gen.ml
@@ -1,26 +1,157 @@
+open Batteries
+;;
+
 (* Unfortunately this Haskell grammar fails very, very badly at being LL(1)
  * (it has lots and lots of infix operators, can't tell tuples from parentheses
  * or lists from list comprehensions, etc.). It might be convertible to LL(1)
  * but that would take tons of factoring. So we'll implement bottom-up parsing.
  * LR(1). *)
 
+type symbol =
+  | Terminal of Parse.term
+  | Nonterminal of Parse.nonterm
+
+type production = {
+  lhs : Parse.nonterm;
+  rhs : symbol list;
+  (* Semantic action to be applied to the list of return values of the
+   * semantic actions of the terms of rhs. *)
+  semantic_action : (Parse.ast list -> Parse.ast);
+}
+
+type grammar = {
+  goal : Parse.nonterm;
+  productions : production Array.t;
+  (* Semantic action to be applied when matching any terminal symbol. Taken as
+   * input the matched lexeme (to extract its contents, line/col #s, etc. *)
+  terminal_action : (Parse.term -> Parse.ast);
+}
+
+(* Types used to construct FIRST sets (and I suppose also FOLLOW sets, except
+ * FOLLOW sets aren't used in LR) *)
+module TermSet =  Set.Make (
+  struct
+    let compare = Pervasives.compare
+    type t = Parse.term
+  end
+)
+
+type first_set = {
+  terms : TermSet.t; (* "Actual" terminals in the set. *)
+  has_epsilon : bool; (* Whether or not the set includes epsilon *)
+}
+
 (* Items in the LR(1) construction *)
 type item = {
   prod : int;
   dot : int;
   lookahead : Parse.term;
-};;
+}
 
 module ItemSet = Set.Make (
   struct
     let compare = Pervasives.compare
     type t = item
   end
-);;
+)
 
-module ItemCollection = Set.Make (
-  struct
-    let compare = Pervasives.compare
-    type t = ItemSet.t * int
+type cc = {
+  itemsets : (int, ItemSet.t) Map.t; (* Store each itemset with an index... *)
+  gotos : (int * Parse.term, int) Map.t; (* so we can look up gotos by index *)
+}
+
+(* States and tables of the resulting pushdown automaton. State numbers will
+ * be the indices of the corresponding itemsets. *)
+type state = int
+
+(* Note that we also need to store the corresponding semantic actions on the
+ * output tree together with each reduce action, so we know what to do with the
+ * "actual stuff" (not just parse terminal / nonterminal types) on the stack *)
+type action =
+  | Shift of state
+  | Reduce of state * (Parse.ast list -> Parse.ast)
+
+type action_table = (state * Parse.term, action) Map.t
+type goto_table = (state * Parse.nonterm, state) Map.t
+
+let first_sets cfg =
+  let curmap = ref Map.empty
+  and all_nonterms =
+      List.unique (Array.to_list (Array.map (fun p -> p.lhs) cfg.productions))
+  in let rec do_nonterm nextnt =
+    if Map.mem nextnt !curmap then
+      ()
+    else begin
+      (* Get all possible RHS productions *)
+      let rhss =
+          Array.map (fun p -> p.rhs)
+                    (Array.filter (fun p -> p.lhs = nextnt) cfg.productions)
+      (* Find the possible first set elems off a given string (symbol list), and
+       * add them into the given first set. Return new first set. *)
+      in let rec add_first_from_str fs symbls =
+        match symbls with
+        (* If we reach empty list, all nonterminals above us in the string had
+         * an epsilon (empty) production. *)
+        | [] -> { fs with has_epsilon = true }
+        | (Terminal t)::_ -> { fs with terms = TermSet.add t fs.terms }
+        | (Nonterminal nt)::ss -> begin
+          (* XXX Grammar had better not be left-recursive (!) *)
+          do_nonterm nt;
+          let firstfs = Map.find nt !curmap in
+          (* If the first symbol is nonterminal (potentially) reducing to
+           * epsilon, we also need to check the 2nd symbol, and so on -
+           * otherwise we can just look at the possible terminals. *)
+          if firstfs.has_epsilon then
+            add_first_from_str
+                { fs with terms = TermSet.union firstfs.terms fs.terms }
+                ss
+          else
+            { fs with terms = TermSet.union firstfs.terms fs.terms }
+        end
+      in
+      (* Find all possible first set elems from productions of this nonterminal
+       * in the grammar. *)
+      let nextfs = Array.fold_left add_first_from_str
+          { terms = TermSet.empty; has_epsilon = false; } rhss
+      in
+      (* And add the resulting first set to the map. ("memoize") *)
+      curmap := Map.add nextnt nextfs !curmap
+    end
+  in begin
+    List.iter do_nonterm all_nonterms;
+    !curmap
   end
-);;
+;;
+
+(* Same as middle loop above, but without memoizing / building table. *)
+let first_set_string fstable symbls =
+  let rec first_set_string_acc syms fs =
+    match syms with
+    | [] -> { fs with has_epsilon = true }
+    | (Terminal t)::_ -> { fs with terms = TermSet.add t fs.terms }
+    | (Nonterminal nt)::ss ->
+      let firstfs = Map.find nt fstable in
+      (* If the first symbol is nonterminal (potentially) reducing to
+       * epsilon, we also need to check the 2nd symbol, and so on -
+       * otherwise we can just look at the possible terminals. *)
+      if firstfs.has_epsilon then
+        first_set_string_acc ss
+            { fs with terms = TermSet.union firstfs.terms fs.terms }
+      else
+        { fs with terms = TermSet.union firstfs.terms fs.terms }
+  in
+  first_set_string_acc symbls
+      { terms = TermSet.empty; has_epsilon = false; }
+;;
+
+let build_cc cfg = { itemsets = Map.empty; gotos = Map.empty }
+;;
+
+let build_tables cfg cc = (Map.empty, Map.empty)
+;;
+
+let simulate cfg acts gotos lexemes = Parse.Foo
+;;
+
+let generate cfg lexemes = Parse.Bar
+;;
--- src/parser_gen.mli
+++ src/parser_gen.mli
@@ -12,7 +12,7 @@ type production = {
   rhs : symbol list;
   (* Semantic action to be applied to the list of return values of the
    * semantic actions of the terms of rhs. *)
-  action : (Parse.ast list -> Parse.ast);
+  semantic_action : (Parse.ast list -> Parse.ast);
 }
 
 type grammar = {
@@ -23,48 +23,63 @@ type grammar = {
   terminal_action : (Parse.term -> Parse.ast);
 }
 
+(* Types used to construct FIRST sets (and I suppose also FOLLOW sets, except
+ * FOLLOW sets aren't used in LR) *)
+module TermSet : Set.S with type elt = Parse.term
+
+type first_set = {
+  terms : TermSet.t; (* "Actual" (non-epsilon) terminals in the set. *)
+  has_epsilon : bool; (* Whether or not the set includes epsilon *)
+}
+
+(* Items in the LR(1) construction *)
+type item = {
+  prod : int; (* Index into given array of productions *)
+  dot : int; (* from 0 up to length of rhs of production *)
+  lookahead : Parse.term;
+}
+
+module ItemSet : Set.S with type elt = item
+
+type cc = {
+  itemsets : (int, ItemSet.t) Map.t; (* Store each itemset with an index... *)
+  gotos : (int * Parse.term, int) Map.t; (* so we can look up gotos by index *)
+}
+
+(* States and tables of the resulting pushdown automaton. State numbers will
+ * be the indices of the corresponding itemsets. *)
+type state = int
+
+(* Note that we also need to store the corresponding semantic actions on the
+ * output tree together with each reduce action, so we know what to do with the
+ * "actual stuff" (not just parse terminal / nonterminal types) on the stack *)
+type action =
+  | Shift of state
+  | Reduce of state * (Parse.ast list -> Parse.ast)
+
+type action_table = (state * Parse.term, action) Map.t
+type goto_table = (state * Parse.nonterm, state) Map.t
+
+(* Compute the FIRST sets for nonterminals in a grammar. *)
+val first_sets : grammar -> (Parse.nonterm, first_set) Map.t
+
+(* Given the computed FIRST sets, find FIRST for an arbitrary input string
+ * (list of symbols). *)
+val first_set_string : (Parse.nonterm, first_set) Map.t ->
+  symbol list -> first_set
+
+(* Build the LR(1) canonical collection for a grammar. *)
+val build_cc : grammar -> cc
+
+(* Use the canonical collection to build the transition tables for the pushdown
+ * automaton. The resulting automaton always has state 0 as the start state. *)
+val build_tables : grammar -> cc -> action_table * goto_table
+
+(* Simulate the resulting pushdown automaton. *)
+val simulate : grammar -> action_table -> goto_table ->
+    Parse.term Queue.t -> Parse.ast
+
 (* Overall function: basically applies the more detailed functions below in the
  * appropriate sequence to build the canonical collection and transition
  * tables, and simulate the resulting automaton. *)
 val generate : grammar -> Parse.term Queue.t -> Parse.ast
-
-(* Items in the LR(1) construction *)
-type item = {
-  prod : int;
-  dot : int;
-  lookahead : Parse.term;
-}
-
-module ItemSet = Set.Make (
-  struct
-    let compare = Pervasives.compare
-    type t = item
-  end
-)
-
-module ItemCollection = Set.Make (
-  struct
-    let compare = Pervasives.compare
-    type t = ItemSet.t * int
-  end
-)
-
-(* States and tables of the resulting pushdown automaton. *)
-type state = int
-
-type action =
-  | Shift of state
-  | Reduce of state
-type action_table = (state * Parse.term, action) Map.t
-type goto_table = (state * Parse.nonterm, state) Map.t
-
-(* Build the LR(1) canonical collection for a grammar. *)
-val build_cc : grammar -> ItemCollection.t
-
-(* Use the canonical collection to build the transition tables for the pushdown
- * automaton. The resulting automaton always has state 0 as the start state. *)
-val build_tables : grammar -> ItemCollection.t -> action_table * goto_table
-
-(* Simulate the resulting pushdown automaton. TODO this isn't quite right - how
- * do we apply the resulting semantic actions? *)
-val simulate : action_table -> goto_table -> Parse.term Queue.t -> Parse.ast
