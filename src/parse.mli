9e90cb1b59c9bf2e4f5b51e8902a9bbb77d3af98
Alvin Kerber <akerber47@gmail.com>
2014-01-10 16:17:18 -0800

    Moved print functions for core types
    In separate file now. Easier to import and use.

--- src/parse.mli
+++ src/parse.mli
@@ -3,6 +3,3 @@ open Types
 
 (* Build an abstract syntax tree for the given stream of lexemes. *)
 val parse : lexeme Queue.t -> ast0
-
-val nonterm_print : 'a BatIO.output -> nonterm -> unit
-val ast0_print : 'a BatIO.output -> ast0 -> unit
---
+++ src/print.mli
@@ -0,0 +1,8 @@
+open Batteries
+open Types
+
+(** For debugging and self-modifying code purposes. *)
+val token_print : 'a BatIO.output -> token -> unit
+val lexeme_print : 'a BatIO.output -> lexeme -> unit
+val nonterm_print : 'a BatIO.output -> nonterm -> unit
+val ast0_print : 'a BatIO.output -> ast0 -> unit
