0bfac67701f9963a4e0ba6c25c41a86f332e0e48
Alvin Kerber <akerber47@gmail.com>
2014-01-12 15:03:10 -0800

    Rewrote lexer tests to reflect later EOF addition
    Now added by unlayout, not postlex

--- test/test_lex.ml
+++ test/test_lex.ml
@@ -52,49 +52,49 @@ let lex_file f =
 
 (* Make sure all token types end / transition correctly. *)
 let test_lex_endtok _ = begin
-  check_toks_cnts "abc123" [VarId; EOF] ["abc123"; ""];
-  check_toks_cnts "123abc" [IntLit; VarId; EOF] ["123"; "abc"; ""];
+  check_toks_cnts "abc123" [VarId] ["abc123"];
+  check_toks_cnts "123abc" [IntLit; VarId] ["123"; "abc"];
   (* Qualified names *)
-  check_toks_cnts "x.y'z" [VarId; VarSym; VarId; EOF] ["x"; "."; "y'z"; ""];
-  check_toks_cnts "X.y'z" [QVarId; EOF] ["X.y'z"; ""];
-  check_toks_cnts "X.Y'z" [QConId; EOF] ["X.Y'z"; ""];
-  check_toks_cnts "X.Y.z" [QConId; VarSym; VarId; EOF] ["X.Y"; "."; "z"; ""];
-  check_toks_cnts "X.Y.Z" [QConId; VarSym; ConId; EOF] ["X.Y"; "."; "Z"; ""];
-  check_toks_cnts "x.Y.Z" [VarId; VarSym; QConId; EOF] ["x"; "."; "Y.Z"; ""];
-  check_toks_cnts "M." [ConId; VarSym; EOF] ["M"; "."; ""];
-  check_toks_cnts "M.." [QVarSym; EOF] ["M.."; ""];
-  check_toks_cnts "M.1" [ConId; VarSym; IntLit; EOF] ["M"; "."; "1"; ""];
-  check_toks_cnts "M...." [QVarSym; EOF] ["M...."; ""];
-  check_toks_cnts "M.:.:" [QConSym; EOF] ["M.:.:"; ""];
+  check_toks_cnts "x.y'z" [VarId; VarSym; VarId] ["x"; "."; "y'z"];
+  check_toks_cnts "X.y'z" [QVarId] ["X.y'z"];
+  check_toks_cnts "X.Y'z" [QConId] ["X.Y'z"];
+  check_toks_cnts "X.Y.z" [QConId; VarSym; VarId] ["X.Y"; "."; "z"];
+  check_toks_cnts "X.Y.Z" [QConId; VarSym; ConId] ["X.Y"; "."; "Z"];
+  check_toks_cnts "x.Y.Z" [VarId; VarSym; QConId] ["x"; "."; "Y.Z"];
+  check_toks_cnts "M." [ConId; VarSym] ["M"; "."];
+  check_toks_cnts "M.." [QVarSym] ["M.."];
+  check_toks_cnts "M.1" [ConId; VarSym; IntLit] ["M"; "."; "1"];
+  check_toks_cnts "M...." [QVarSym] ["M...."];
+  check_toks_cnts "M.:.:" [QConSym] ["M.:.:"];
   (* Names and operators *)
-  check_toks_cnts "x+1" [VarId; VarSym; IntLit; EOF] ["x"; "+"; "1"; ""];
-  check_toks_cnts "x+-1" [VarId; VarSym; IntLit; EOF] ["x"; "+-"; "1"; ""];
-  check_toks_cnts "x + -1" [VarId; VarSym; VarSym; IntLit; EOF]
-    ["x"; "+"; "-"; "1"; ""];
+  check_toks_cnts "x+1" [VarId; VarSym; IntLit] ["x"; "+"; "1"];
+  check_toks_cnts "x+-1" [VarId; VarSym; IntLit] ["x"; "+-"; "1"];
+  check_toks_cnts "x + -1" [VarId; VarSym; VarSym; IntLit]
+    ["x"; "+"; "-"; "1"];
   (* Integer literals *)
-  check_toks_cnts "0123" [IntLit; EOF] ["0123"; ""];
-  check_toks_cnts "0o123" [IntLit; EOF] ["0o123"; ""];
-  check_toks_cnts "0x123" [IntLit; EOF] ["0x123"; ""];
-  check_toks_cnts "0xdef" [IntLit; EOF] ["0xdef"; ""];
-  check_toks_cnts "0odef" [IntLit; VarId; EOF] ["0"; "odef"; ""];
-  check_toks_cnts "0o189def" [IntLit; IntLit; VarId; EOF]
-    ["0o1"; "89"; "def"; ""];
-  check_toks_cnts "0xdefghi" [IntLit; VarId; EOF] ["0xdef"; "ghi"; ""];
-  check_toks_cnts "0xghi" [IntLit; VarId; EOF] ["0"; "xghi"; ""];
+  check_toks_cnts "0123" [IntLit] ["0123"];
+  check_toks_cnts "0o123" [IntLit] ["0o123"];
+  check_toks_cnts "0x123" [IntLit] ["0x123"];
+  check_toks_cnts "0xdef" [IntLit] ["0xdef"];
+  check_toks_cnts "0odef" [IntLit; VarId] ["0"; "odef"];
+  check_toks_cnts "0o189def" [IntLit; IntLit; VarId]
+    ["0o1"; "89"; "def"];
+  check_toks_cnts "0xdefghi" [IntLit; VarId] ["0xdef"; "ghi"];
+  check_toks_cnts "0xghi" [IntLit; VarId] ["0"; "xghi"];
   (* Floating point literals *)
-  check_toks_cnts "1.2" [FloatLit; EOF] ["1.2"; ""];
-  check_toks_cnts "1..2" [IntLit; RDotDot; IntLit; EOF] ["1"; ".."; "2"; ""];
-  check_toks_cnts "1.2e" [FloatLit; VarId; EOF] ["1.2"; "e"; ""];
-  check_toks_cnts "1.2e+" [FloatLit; VarId; VarSym; EOF] ["1.2"; "e"; "+"; ""];
-  check_toks_cnts "1.2e3" [FloatLit; EOF] ["1.2e3"; ""];
-  check_toks_cnts "1.2e+03" [FloatLit; EOF] ["1.2e+03"; ""];
-  check_toks_cnts "1.2E-3" [FloatLit; EOF] ["1.2E-3"; ""];
-  check_toks_cnts "1.2e-3+4" [FloatLit; VarSym; IntLit; EOF]
-    ["1.2e-3"; "+"; "4"; ""];
-  check_toks_cnts "1.2e0f0" [FloatLit; VarId; EOF] ["1.2e0"; "f0"; ""];
-  check_toks_cnts "0x1.2" [IntLit; VarSym; IntLit; EOF] ["0x1"; "."; "2"; ""];
-  check_toks_cnts "1.0x2" [FloatLit; VarId; EOF] ["1.0"; "x2"; ""];
-  check_toks_cnts "1.2e+0x2" [FloatLit; VarId; EOF] ["1.2e+0"; "x2"; ""];
+  check_toks_cnts "1.2" [FloatLit] ["1.2"];
+  check_toks_cnts "1..2" [IntLit; RDotDot; IntLit] ["1"; ".."; "2"];
+  check_toks_cnts "1.2e" [FloatLit; VarId] ["1.2"; "e"];
+  check_toks_cnts "1.2e+" [FloatLit; VarId; VarSym] ["1.2"; "e"; "+"];
+  check_toks_cnts "1.2e3" [FloatLit] ["1.2e3"];
+  check_toks_cnts "1.2e+03" [FloatLit] ["1.2e+03"];
+  check_toks_cnts "1.2E-3" [FloatLit] ["1.2E-3"];
+  check_toks_cnts "1.2e-3+4" [FloatLit; VarSym; IntLit]
+    ["1.2e-3"; "+"; "4"];
+  check_toks_cnts "1.2e0f0" [FloatLit; VarId] ["1.2e0"; "f0"];
+  check_toks_cnts "0x1.2" [IntLit; VarSym; IntLit] ["0x1"; "."; "2"];
+  check_toks_cnts "1.0x2" [FloatLit; VarId] ["1.0"; "x2"];
+  check_toks_cnts "1.2e+0x2" [FloatLit; VarId] ["1.2e+0"; "x2"];
 end
 ;;
 
