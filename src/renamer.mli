6981477b5b642d9cc07eb14dfb279a992cc89cec
Alvin Kerber <akerber47@gmail.com>
2014-04-24 15:20:58 -0700

    Fixed up interface a bit
    Now matches things actually implemented

--- src/renamer.mli
+++ src/renamer.mli
@@ -5,10 +5,6 @@ open Types
  * actual name. Store None as value in case of ambiguity. *)
 type environment = (prename, name option) Map.t
 
-(* Convert lexeme to prename. Namespace is context-dependent so can't be
- * determined from lexeme. *)
-val lx_to_pn : lexeme -> namespace -> prename
-
 (** Take in an ast and gather all the top-level names. These could be imported
  * or defined at the top-level of this module. *)
 val build_globals : ast1 -> environment
@@ -18,9 +14,3 @@ val build_globals : ast1 -> environment
  * go. *)
 val rename : environment -> ast1 -> ast1
 
-(** Using operator fixity declarations, restructure all infixexps and
- * infixpats. This will change each infixexp/pat_op
- * from having one exp/pat10 child and one infixexp/pat child
- * to having two infixexp/pat children. Takes in a renamed ast (ie one with
- * only rleaf leaves) *)
-val resolve_fixities : ast1 -> ast1
